b15cc76e90702e940fceb1429f0ca8369da51fba
AngerOverApathy <violasian@gmail.com>
2023-11-23 16:16:29 -0800

    fixed location icon

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -7,11 +7,11 @@ export default function Cards(props) {
             <img src={props.imageUrl} className='thumbnail' alt='cover image'/>
             <div className='card-info'>
                 <div className='base-info'>
-                    <img src={locationIcon} alt="location icon" />
+                    <img src={locationIcon} className='location-icon' alt="location icon" />
                     <p className='location'>{props.location}</p>
-                    <h3 className='attraction'>{props.title}</h3>
-                    <p className='maps'>{props.googleMapsUrl}</p>
+                    <a href={props.googleMapsUrl} className='maps'>View on Google Maps</a>
                 </div>
+                <h3 className='title'>{props.title}</h3>
                 <p className='dates'>{props.startDate} - {props.endDate}</p>
                 <p>{props.description}</p>
             </div>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className='banner'>
             <img className='icon' src={worldIcon} alt="world icon" />
-            <span>The Wandering Dancer</span>
+            <span>The Wanderer</span>
         </header>
     )
     }
